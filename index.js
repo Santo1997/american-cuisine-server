704b20b2f0d180774466e29a59e62b96b46efaee
Santo1997 <hossainsanto2@gmail.com>
2023-05-03 01:14:08 +0600

    vercel setup

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000;
 
-
 app.use(cors())
 
 const recipeData =  require('./data/recipies.json');
@@ -27,14 +26,11 @@ app.get('/chefData', (req, res) => {
   res.send(chefData);
 })
 
-
 app.get('/chefData/:id', (req, res) => {
    const chefId = req.params.id;
   const chefInfo = chefData?.find((chef) => chef.id == parseInt(chefId));
   res.send(chefInfo);
 })
-
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
