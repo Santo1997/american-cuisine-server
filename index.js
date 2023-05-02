a650e057dd80234cce34662d254da659e3a83eb2
Santo1997 <hossainsanto2@gmail.com>
2023-05-03 01:00:05 +0600

    get single recipe data

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ app.get('/recipies', (req, res) => {
   res.send(recipeData);
 })
 
+app.get('/recipies/:id', (req, res) => {
+  const id = req.params.id;
+  const itms = recipeData?.find((itm) => itm.recipe_id == id);
+  res.send(itms);
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
