2b7d292c1374579b5879134b46648c55ac05bbad
Santo1997 <hossainsanto2@gmail.com>
2023-05-03 00:57:52 +0600

    set cors & recipies

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
-
+var cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000;
 
+
+app.use(cors())
+
+const recipeData =  require('./data/recipies.json');
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/recipies', (req, res) => {
+  res.send(recipeData);
 })
 
 
